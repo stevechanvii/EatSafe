64607a820d5ec141eb9f3719e7f1ec638346300b
stevechanvii <dche0007@student.monash.edu>
2019-04-06 13:16:28 +1100

    bug fix for android

--- src/Navigation/ScanningScreen/ScanningScreen.js
+++ src/Navigation/ScanningScreen/ScanningScreen.js
@@ -1,19 +1,3 @@
-// import React, { Component } from 'react';
-
-// import { Text, View } from 'react-native';
-
-// class scanningScreen extends Component {
-//     render() {
-//         return (
-//             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//                 <Text>Scanning!</Text>
-//             </View>
-//         );
-//     }
-// }
-
-// export default scanningScreen;
-
 'use strict';
 import React, { Component } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
