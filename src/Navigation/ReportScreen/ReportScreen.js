70c9b212c657ea4e9b13d2aa3ad1abaeda6fe64f
stevechanvii <dche0007@student.monash.edu>
2019-04-06 14:18:44 +1100

    Set up NativeBase

--- src/Navigation/ReportScreen/ReportScreen.js
+++ src/Navigation/ReportScreen/ReportScreen.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
-import { Text, View } from 'react-native';
+// import { Text, View } from 'react-native';
+import { Container, Header, Content, Accordion } from "native-base";
+
+
+const dataArray = [
+    { title: "First Element", content: "1 Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet" },
+    { title: "Second Element", content: "2 Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet" },
+    { title: "Third Element", content: "3 Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet" }
+];
 
 class reportScreen extends Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Report!</Text>
-            </View>
+            <Container>
+                <Header />
+                <Content padder>
+                    <Accordion
+                        dataArray={dataArray}
+                        icon="add"
+                        expandedIcon="remove"
+                        iconStyle={{ color: "green" }}
+                        expandedIconStyle={{ color: "red" }}
+                    />
+                </Content>
+            </Container>
         );
     }
 }
