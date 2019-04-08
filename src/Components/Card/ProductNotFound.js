6268c60997cdc26c6c2153cced08919deee428dd
stevechanvii <dche0007@student.monash.edu>
2019-04-09 00:48:09 +1000

    change UI of product not found

--- src/Components/Card/ProductNotFound.js
+++ src/Components/Card/ProductNotFound.js
@@ -5,9 +5,9 @@ import { Container, Header, Content, Thumbnail, Text, Button, Badge, Body, ListI
 
 const productNotFound = (props) => (
     <Container style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Icon name='comment-alert-outline' size={40} color={tintColor} />
-        <Text style={{ color: "black", fontSize: 20, fontWeight: "bold" }}>Barcode: {props.barcode}</Text>
-        <Text style={{ color: "black", fontSize: 18 }}>Sorry, product not found</Text>
+        <Icon name='comment-alert-outline' size={40} />
+        <Text style={{ color: "black", fontSize: 20, fontWeight: "bold" }}>Sorry, product not found!</Text>
+        <Text style={{ color: "black", fontSize: 18 }}>Barcode: {props.barcode}</Text>
     </Container>
 );
 
